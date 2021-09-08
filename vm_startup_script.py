5f2c825e30aae758001c1d157071f16cb20f9343
Jordan Juras <jordan.juras@native-instruments.de>
2021-09-08 14:59:51 +0200

    Use tmux to run training, dump log to a file, define optional args

--- vm_startup_script.py
+++ vm_startup_script.py
@@ -2,49 +2,66 @@ from argparse import ArgumentParser
 from pathlib import Path
 from subprocess import call
 
+
 def get_args():
     parser = ArgumentParser(description='Configure training environment for StyleGAN2-ADA')
 
     parser.add_argument('--bucket_audio_data_dir'
                         , type=str
-                        , default=''
-                        , help='')
-
-    parser.add_argument('--local_audio_data_dir'
-                        , type=str
-                        , default=''
-                        , help='')
-
-    parser.add_argument('--local_dataset_dir'
-                        , type=str
-                        , default=''
+                        , required=True
                         , help='')
 
     parser.add_argument('--train_config'
                         , type=str
-                        , default='drumgan'
+                        , required=True
                         , help='')
+
+    # optional arguments
+    parser.add_argument('--local_audio_data_dir'
+                        , type=str
+                        , required=True
+                        , default='audio/'
+                        , help='')
+
+    parser.add_argument('--local_dataset_dir'
+                        , type=str
+                        , required=True
+                        , default='dataset/'
+                        , help='')
+
+    parser.add_argument('--local_saved_training_data_dir'
+                    , type=str
+                    , required=True
+                    , default='saved_training_data/'
+                    , help='')
+
 
     return parser.parse_args()
 
 
 def install_dependencies():
-    call("sh bootstrap/bootstrap.sh".split(" "))
+    call('sh bootstrap/bootstrap.sh'.split(' '))
 
 
 def copy_audio_data_from_bucket(source_dir: str, dest_dir: str):
     if not Path(dest_dir).exists():
         Path(dest_dir).mkdir()
         
-    call(f"gsutil -m cp -r gs://{source_dir}/* {dest_dir}".split(" "))
+    call(f'gsutil -m cp -r gs://{source_dir}/* {dest_dir}'.split(' '))
 
 
 def render_audio_data_to_dataset(audio_data_dir: str, dataset_dir: str):
-    call(f"python3.8 dataset_tool_audio.py --source {audio_data_dir} --dest {dataset_dir}".split(" "))
+    call(f'python3.8 dataset_tool_audio.py --source {audio_data_dir} --dest {dataset_dir}'.split(' '))
 
 
 def train_model(dataset_dir: str, training_data_out_dir: str, train_config: str):
-    call(f"python3.8 train.py --outdir {training_data_out_dir} --data {dataset_dir} --gpus 1 --cfg {train_config}".split(" "))
+    Path('setup_log.log').touch()
+    training_log = open('training_log.log', 'w')
+
+    training_cmd = f'python3.8 train.py --outdir {training_data_out_dir} --data {dataset_dir} --gpus 1 --cfg {train_config}'.split(' ')
+
+    call(f'tmux new -d -s {train_config}_training'.split(' '), stdout=training_log, stderr=training_log)
+    call(['tmux', 'send-keys', '-t', f'{train_config}_training', f'{training_cmd}', 'Enter'], stdout=training_log, stderr=training_log)
 
 
 if __name__ == "__main__":
@@ -54,4 +71,4 @@ if __name__ == "__main__":
     install_dependencies()
     copy_audio_data_from_bucket(setup_args.bucket_audio_data_dir, setup_args.local_audio_data_dir)
     render_audio_data_to_dataset(setup_args.local_audio_data_dir, setup_args.local_dataset_dir)
-    train_model(setup_args.local_dataset_dir, "saved_training_data/", setup_args.train_config)
+    train_model(setup_args.local_dataset_dir, setup_args.local_saved_training_data_dir, setup_args.train_config)

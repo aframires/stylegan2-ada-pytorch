3e3d3d9a6b4b7f879b3fbbfb22cf187b2f6936e1
aframires <aframires@gmail.com>
2022-04-12 15:09:43 +0100

    website

--- bootstrap/bootstrap.sh
+++
@@ -1,2 +0,0 @@
-python3.8 -m pip install -r bootstrap/requirements.txt
-apt install -y libsndfile1
--- sefa_demo.py
+++
@@ -1,4 +0,0 @@
-import demo.generator.BoundGenApp as BGAPP
-
-if __name__ == "__main__":
-    BGAPP.run()
